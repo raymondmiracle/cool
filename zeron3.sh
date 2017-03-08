78cfb595a993728668b0e711cb4b53d46cf5bbff
walter79 <gzwalter79@gmail.com>
2017-03-08 21:03:09 +0100

    Linux 3.18.37
    Signed-off-by: Mohancm100 <mohancm100@gmail.com>

--- zeron3.sh
+++ zeron3.sh
@@ -12,7 +12,7 @@ export SUBARCH=arm64
 export KBUILD_BUILD_USER="subzero"
 export KBUILD_BUILD_HOST="zerohost"
 export CROSS_COMPILE=/mnt/kernel_stuff/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-make CP8676_I02_defconfig ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
+make CP8676_I02_defconfig -j8 ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
 
 
